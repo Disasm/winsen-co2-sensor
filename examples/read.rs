c84cfd13d64eb246266bd6bd9ced06800a7ce22c
Vadim Kaushan <admin@disasm.info>
2020-02-22 17:18:42 +0300

    Add examples

---
+++ examples/read.rs
@@ -0,0 +1,17 @@
+use linux_embedded_hal::Serial;
+use winsen_co2_sensor::WinsenSensor;
+use std::time::Instant;
+
+fn main() {
+    let serial_path = std::env::args().nth(1).unwrap_or("/dev/ttyUSB0".into());
+    let serial = Serial::open(serial_path).unwrap();
+    let mut sensor = WinsenSensor::new(serial, Instant::now());
+
+    let probe_ok = sensor.probe().unwrap();
+    println!("Probe: {:?}", probe_ok);
+    if !probe_ok {
+        return;
+    }
+
+    println!("CO2: {}", sensor.read_co2_concentration().unwrap());
+}
